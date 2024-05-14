13583d0afcead1060fbf785d1dc478cf9ce02a7c
MD-HABIB-ULLA <habibulla1278@gmail.com>
2024-05-14 16:50:27 +0600

    last thing added

--- src/Pages/BorrowedBooks/BorrowedBooks.jsx
+++ src/Pages/BorrowedBooks/BorrowedBooks.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
+import toast from "react-hot-toast";
 
 const BorrowedBooks = () => {
   const [bookData, setBookData] = useState([]);
@@ -10,11 +11,38 @@ const BorrowedBooks = () => {
   //   console.log(email);
   useEffect(() => {
     axios
-      .get(`http://localhost:4000/borrowedBooks/${email}`)
-      .then((res) => setBookData(res.data));
+      .get(`https://book-zone-server.vercel.app/borrowedBooks/${email}`)
+      .then((res) => setBookData(res.data))
+      .catch((err) => console.log(err));
   }, [email]);
+
+  const handleReturnBook = (book) => {
+    const bookId = book._id;
+    const updataId = book.book._id;
+    console.log(updataId);
+    const returnBook = bookData.filter((book) => book._id !== bookId);
+    axios
+      .delete(`https://book-zone-server.vercel.app/returnBook/${bookId}`)
+      .then((res) => {
+        if (res.data.deletedCount === 1) {
+          axios
+            .post(
+              `https://book-zone-server.vercel.app/returnBookQuantity/${updataId}`
+            )
+            .then((res) => {
+              if (res.data.acknowledged) {
+                setBookData(returnBook);
+                toast.success("Returned the book");
+              }
+            })
+            .catch((err) => console.log(err));
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <div className="flex flex-wrap flex-col items-center justify-center">
+    <div className="flex flex-wrap flex-col items-center mt-20 min-h-screen">
       {bookData.map((book) => (
         <div
           key={book._id}
@@ -23,7 +51,7 @@ const BorrowedBooks = () => {
           <img
             src={book.book.bookData.image}
             alt={book.book.bookData.name}
-            className=" h-48 "
+            className=" h-48 w-36"
           />
           <div className="p-4 flex-1">
             <p className="text-xl font-semibold">{book.book.bookData.name}</p>
@@ -31,8 +59,8 @@ const BorrowedBooks = () => {
             <p className="text-gray-600">Borrowed Date: {book.todayDate}</p>
             <p className="text-gray-600">Return Date: {book.returnDate}</p>
             <button
-              //   onClick={() => handleReturnBook(book._id)}
-              className="mt-2 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+              onClick={() => handleReturnBook(book)}
+              className="mt-2  text-white font-bold border-none btn bg-gradient-to-r from-[#9e24b2] to-[#4724b2] uppercase"
             >
               Return
             </button>
