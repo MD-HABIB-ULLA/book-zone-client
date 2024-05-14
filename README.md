ae564b47726c6269ff3afee4f8faec4ec4e571b3
MD-HABIB-ULLA <habibulla1278@gmail.com>
2024-05-15 00:11:01 +0600

    done

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
+# BookZone
+
+## Live Site URL
+
+[BookZone Live](https://bookzone-7c036.web.app/)
+
+## Category
+
+assignment_category_0008
+
+## Description
+
+BookZone is an online platform where users can browse, borrow, and review books from a diverse collection organized by separate categories. It provides a convenient and user-friendly experience for book lovers to explore new reads and share their opinions.
+
+## Features
+
+- Browse and borrow books from a diverse collection organized by separate categories.
+- Share opinions and reviews on books.
+- Easy borrowing process with a simple click.
+- Convenient return option after reading.
+- Update route available for modifying book information.
+
+## Technologies Used
+
+- React.js
+- Express.js
+- MongoDB
+- Axios
+- Tailwind CSS
+
+
+
+
+
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- src/Components/UpcomingEvents/UpcomingEvents.jsx
+++ src/Components/UpcomingEvents/UpcomingEvents.jsx
@@ -8,8 +8,8 @@ const UpcomingEvents = () => {
         }}
         className=" bg-center bg-no-repeat bg-cover"
       >
-        <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 dark:text-gray-50">
-          <h1 className="text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl dark:text-gray-50">
+        <div className="container flex flex-col items-center px-4 py-16 pb-24 mx-auto text-center lg:pb-56 md:py-32 md:px-10 lg:px-32 text-gray-50">
+          <h1 className="text-5xl font-bold leading-none sm:text-6xl xl:max-w-3xl text-gray-50">
             Share Your Ideas
           </h1>
           <p className="mt-6 mb-8 text-lg sm:mb-12 xl:max-w-3xl dark:text-gray-50">
@@ -32,13 +32,13 @@ const UpcomingEvents = () => {
       </div>
       <section className="w-5/6 mx-auto mb-12 -mt-20  rounded-lg shadow-md lg:-mt-40">
         <div className="max-w-6xl mx-auto px-4">
-          <h2 className=" md:text-5xl text-2xl text-center bg-[#24232365] dark:text-white text-gray-800 mb-8 font-bold">
+          <h2 className=" md:text-5xl text-2xl text-center bg-[#24232365] text-white  mb-8 font-bold">
             Upcoming Events
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {/* Event 1 */}
             <div className="bg-white shadow-lg rounded-lg p-6">
-              <h3 className="text-xl font-semibold text-gray-800 mb-2">
+              <h3 className="text-xl font-semibold  mb-2">
                 Book Club Meeting
               </h3>
               <p className="text-gray-600 mb-4">Date: September 15, 2024</p>
