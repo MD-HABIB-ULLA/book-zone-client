71a1a9260891d8b258c0e3ad2c9bbbaa14e39ebd
habib ulla <habibulla1278@gmail.com>
2024-08-21 10:58:58 +0600

    navber editing

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,19 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navber from "../Components/Navber/Navber";
-import { Toaster } from "react-hot-toast";
+import { LoaderIcon, Toaster } from "react-hot-toast";
 import Footer from "../Components/Footer/Footer";
 
-
 const Root = () => {
-    return (
-        <div className="dark:bg-[#212121] bg-[#F6F9FC] duration-500">
-             <Toaster position="top-center" reverseOrder={false} />
-            <Navber/>
-            <Outlet/>
-            <Footer/>
-          
-        </div>
-    );
+  const location = useLocation();
+  console.log(location);
+  return (
+    <div className="dark:bg-[#212121] bg-[#F6F9FC] duration-500">
+      <Toaster position="top-center" reverseOrder={false} />
+      {location.pathname !== "/" && <Navber />}
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default Root;
