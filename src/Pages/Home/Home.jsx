4a0aab6d12e7d9730db5e20c000d156fd819b34d
habib ulla <habibulla1278@gmail.com>
2024-08-24 12:36:04 +0600

    updated

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,7 +5,6 @@ import GetUpdate from "../../Components/GetUpdate/GetUpdate";
 import LibraryServices from "../../Components/LibraryServices/LibraryServices";
 import Reviews from "../../Components/Reviews/Reviews";
 import SwiperBanner from "../../Components/Swiper/Swiper";
-import UpcomingEvents from "../../Components/UpcomingEvents/UpcomingEvents";
 
 const Home = () => {
   return (
