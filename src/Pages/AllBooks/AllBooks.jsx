64b84287f8c2e844e849963ee3ace44f1c6ba60b
habib ulla <habibulla1278@gmail.com>
2024-08-24 12:27:03 +0600

    updated

--- src/Pages/AllBooks/AllBooks.jsx
+++ src/Pages/AllBooks/AllBooks.jsx
@@ -132,13 +132,7 @@ const AllBooks = () => {
                         readonly
                       />
                     </div>
-                    {/* <Link
-                      to={`/update/${book._id}`}
-                      className=" text-white font-bold border-none btn bg-gradient-to-r from-[#9e24b2] to-[#4724b2] uppercase"
-                    >
-                      {" "}
-                      Update
-                    </Link> */}
+                   
                   </div>
                 </Link>
               ))}
